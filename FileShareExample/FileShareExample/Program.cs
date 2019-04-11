c4448e5f31473fecbe4ec8cc316230235b6bbb0b
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2019-04-11 11:18:08 -0400

    fileshare enum example

---
+++ FileShareExample/FileShareExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileShareExample
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            using (var writer = new StreamWriter(new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None)))
+            {
+                Task.WaitAll(Task.Factory.StartNew(() => WriteToFile("thread1", writer)),
+                             Task.Factory.StartNew(() => WriteToFile("thread2", writer)));
+            }
+        }
+
+        static void WriteToFile(string threadName, StreamWriter writer)
+        {
+            for (var i = 0; i < 100; i++)
+                writer.WriteLine($"[{DateTime.Now.ToUniversalTime()} : {threadName}]: {Guid.NewGuid()}\r\n");
+        }
+    }
+}
