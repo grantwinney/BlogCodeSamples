4a6cf48499613eb10461e8b680976a8003a99250
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2020-04-05 13:50:20 -0400

    merge separate repo for blog post into this one

---
+++ TourVisualStudioForMac/README.md
@@ -0,0 +1,2 @@
+# visual-studio-for-mac-sandbox
+Experimenting with the capabilities of Visual Studio for Mac
---
+++ TourVisualStudioForMac/vs4mac_dotnet_standard_library/vs4mac_dotnet_standard_library/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace vs4mac_dotnet_standard_library
+{
+    public static class Magicmatics
+    {
+        public static int GimmeRandomNumber()
+        {
+            return 4;  // chosen by fair dice roll.
+                       // guaranteed to be random.
+        }
+    }
+}
